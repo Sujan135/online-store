f1319fc316c346bb95fb2f8d9878a77180dac974
Sujan Banjara <banjara.suzan12@gmail.com>
2025-04-25 15:27:06 -0500

    Load products from CSV in ProductLoader

---
+++ OnlineStoreApp/src/main/java/com/pluralsight/ProductLoader.java
@@ -0,0 +1,39 @@
+package com.pluralsight;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ProductLoader {
+        public static ArrayList<Product> loadProducts(String filename) {
+        ArrayList<Product> inventory = new ArrayList<>();
+
+        try {
+            String filePath = "src/main/resources/DataFiles/products.csv";
+            FileReader fileReader = new FileReader(filePath);
+            BufferedReader reader = new BufferedReader(fileReader);
+
+            String line;
+            while((line = reader.readLine()) !=null) {
+                String[] parts = line.split("\\|");
+                String sku = parts[0];
+                String name = parts[1];
+                double price = Double.parseDouble(parts[2]);
+                String department = parts[3];
+                Product product = new Product(sku, name, price, department);
+                inventory.add(product);
+
+            }
+
+        }catch(IOException e) {
+            System.out.println("Error reading file: " + e.getMessage());
+            e.printStackTrace();
+        }
+        return inventory;
+
+    }
+
+}
