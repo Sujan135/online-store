03e4da2edffe47f6499af0de7e4db08ba4dea6a7
Sujan Banjara <banjara.suzan12@gmail.com>
2025-04-28 12:11:05 -0500

    Add UserInterface class for user interaction

--- OnlineStoreApp/src/main/java/com/pluralsight/UserInterface.java
+++ OnlineStoreApp/src/main/java/com/pluralsight/UserInterface.java
@@ -1,5 +1,6 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -7,27 +8,44 @@ public class UserInterface {
     Scanner scanner = new Scanner(System.in);
 
 
-    public void showMainMenu() {
-        while(true) {
+    public int showMainMenu() {
             System.out.println("\nWelcome to the Online Store");
             System.out.println("1. Display Products");
             System.out.println("2. Display cart");
             System.out.println("3. Exit");
             System.out.println("Choose an option: ");
-            String input = scanner.nextLine();
+            return scanner.nextInt();
+    }
 
-            switch (input) {
-                case "1":
-                    break;
-                case "2":
-                    break;
-                case "3":
-                    break;
-                default:
-                    System.out.println("Invalid input. Please try again.");
-            }
-
+    public void displayProducts(ArrayList<Product> products) {
+        System.out.println("\nProducts");
+        for (Product product : products) {
+            System.out.println(product);
         }
     }
+
+    public String forSkuToAdd()  {
+        System.out.println("Enter SKU to add to cart: ");
+        return scanner.next();
+    }
+    public int showProductsMenu() {
+        System.out.println("\n1. Search Products");
+        System.out.println("2. Add Product to Cart");
+        System.out.println("3. Back to Home");
+        System.out.print("Choose an option: ");
+        return scanner.nextInt();
+    }
+
+    public int showCartMenu() {
+        System.out.println("\n1. Remove Product from Cart");
+        System.out.println("2. Checkout");
+        System.out.println("3. Back to Home");
+        System.out.print("Choose an option: ");
+        return scanner.nextInt();
+    }
+
+
+
+
 
 }
